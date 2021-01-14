1e387fa14fecfe74971d68467d6ae4181b234783
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-01-14 06:56:04 -0800

    Better clarification and theme fix

--- Tampermonkey-Scripts/Degree Audit Reporting System/PSU-DARS-Clarified-Credits-Earned.user.js
+++ Tampermonkey-Scripts/Degree Audit Reporting System/PSU-DARS-Clarified-Credits-Earned.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PSU - DARS Clarified Credits Earned
 // @namespace    https://thealiendrew.github.io/
-// @version      1.0.1
+// @version      1.1.0
 // @description  This will show the total number of credits earned, and how much of the earned credits were taken from PSU or external sources, along with showing the total amount from external sources (which may not have been transferred).
 // @author       AlienDrew
 // @match        https://app.banner.pdx.edu/uachieve_selfservice/audit/read.html*
@@ -17,9 +17,7 @@ let allCoursesWithDuplicatesAndZeroCreditAndInProgress = [];
 let allCoursesAndZeroCreditAndInProgress = [];
 let allCoursesAndInProgress = [];
 let finishedCourses = [];
-let externalUni = {courses: 0, credits: 0};
-let uni = {fromUni: {courses: 0, credits: 0}, fromTransfer: {courses: 0, credits: 0}};
-let total = {used: {courses: 0, credits: 0}, unused: {courses: 0, credits: 0}};
+let total = {all: {courses: 0, credits: 0}, uni: {courses: 0, credits: 0}, precedesTransferGrade: {courses: 0, credits: 0}, noTransferGradeUsed: {courses: 0, credits: 0}, transferred: {courses: 0, credits: 0}};
 
 /* A course object
  *
@@ -106,52 +104,85 @@ for (let i = 0; i < finishedCourses.length; i++) {
     let checkingCourse = finishedCourses[i];
     let courseCredit = checkingCourse.credit;
 
+    total.all.courses++;
+    total.all.credits += courseCredit;
     if (checkingCourse.grade == '--') {
-        total.unused.courses++;
-        total.unused.credits += courseCredit;
-        externalUni.courses++;
-        externalUni.credits += courseCredit;
+        total.noTransferGradeUsed.courses++;
+        total.noTransferGradeUsed.credits += courseCredit;
+        total.transferred.courses++;
+        total.transferred.credits += courseCredit;
     } else if (checkingCourse.grade.includes('T')) {
-        total.used.courses++;
-        total.used.credits += courseCredit;
-        externalUni.courses++;
-        externalUni.credits += courseCredit;
-        uni.fromTransfer.courses++;
-        uni.fromTransfer.credits += courseCredit;
+        total.precedesTransferGrade.courses++;
+        total.precedesTransferGrade.credits += courseCredit;
+        total.transferred.courses++;
+        total.transferred.credits += courseCredit;
     } else {
-        total.used.courses++;
-        total.used.credits += courseCredit;
-        uni.fromUni.courses++;
-        uni.fromUni.credits += courseCredit;
+        total.uni.courses++;
+        total.uni.credits += courseCredit;
     }
 }
 
-// display results
+// display results (which accounts for darkreader)
 let colorStyleLight = 'color: rgb(255, 255, 255); background-color: rgba(0, 0, 0, 0.667)'
 let colorStyleDark = 'color: rgb(0, 0, 0) !important; background-color: rgba(255, 255, 255, 0.667) !important'
-let divStyleLight = 'display: inline-block; position: fixed; bottom: 10px; right: 10px; padding: 10px; border-radius: 10px; ' + colorStyleLight;
-let divStyleDark = 'display: inline-block; position: fixed; bottom: 10px; right: 10px; padding: 10px; border-radius: 10px; ' + colorStyleDark;
-let pStart = '<p style="margin: 0px !important; font-size: 10pt;">';
+let childColorStyleLight = 'color: rgb(255, 255, 255); background-color: transparent !important'
+let childColorStyleDark = 'color: rgb(0, 0, 0) !important; background-color: transparent !important'
+let divStyleBase = 'display: inline-block; position: fixed; bottom: 10px; right: 10px; padding: 10px; border-radius: 10px; ';
+let divStyleLight = divStyleBase + colorStyleLight;
+let divStyleDark = divStyleBase + colorStyleDark;
+let pStyleBase = 'margin: 0px !important; font-size: 10pt; ';
+let pStyleLight = pStyleBase + childColorStyleLight;
+let pStyleDark = pStyleBase + childColorStyleDark;
+let pStartLight = '<p style="' + pStyleLight + '">';
+let pStartDark = '<p style="' + pStyleDark + '">';
 let pEnd = '</p>';
-let titleStart = '<b style="font-size: 125%"><u>';
+let titleStyleBase = 'font-size: 125%; ';
+let titleStyleLight = titleStyleBase + childColorStyleLight;
+let titleStyleDark = titleStyleBase + childColorStyleDark;
+let titleStartLight = '<b style="' + titleStyleLight + '"><u>';
+let titleStartDark = '<b style="' + titleStyleDark + '"><u>';
 let titleEnd = '</b></u>';
-let contentStart = '<i>';
+let contentStartLight = '<i style="' + childColorStyleLight + '">';
+let contentStartDark = '<i style="' + childColorStyleDark + '">';
 let contentEnd = '</i>';
 let newline = '<br>';
-let divInnerHTML = pStart+titleStart+ 'External colleges/universities' +titleEnd+newline+contentStart +externalUni.credits+' credits (from '+externalUni.courses+' courses)' +contentEnd+pEnd+newline +
-                   pStart+titleStart+ 'Not used in Degree at PSU' +titleEnd+newline+contentStart +total.unused.credits+' credits (from '+total.unused.courses+' courses)' +contentEnd+pEnd +
-                   pStart+titleStart+ 'Transferred to PSU' +titleEnd+newline+contentStart +uni.fromTransfer.credits+' credits (from '+uni.fromTransfer.courses+' courses)' +contentEnd+pEnd +
-                   pStart+titleStart+ 'Earned at PSU' +titleEnd+newline+contentStart +uni.fromUni.credits+' credits (from '+uni.fromUni.courses+' courses)' +contentEnd+pEnd+newline +
-                   pStart+titleStart+ 'PSU Total for Degree' +titleEnd+newline+contentStart +total.used.credits+' credits (from '+total.used.courses+' courses)' +contentEnd+pEnd;
+let divInnerHTML = pStartLight+titleStartLight+ 'Transferred' +titleEnd+newline+contentStartLight +total.transferred.credits+' credits (from '+total.transferred.courses+' courses)' +contentEnd+pEnd +
+                   pStartLight+titleStartLight+ 'No Transfer Grade Used' +titleEnd+newline+contentStartLight +total.noTransferGradeUsed.credits+' credits (from '+total.noTransferGradeUsed.courses+' courses)' +contentEnd+pEnd +
+                   pStartLight+titleStartLight+ 'Precedes Transfer Grade' +titleEnd+newline+contentStartLight +total.precedesTransferGrade.credits+' credits (from '+total.precedesTransferGrade.courses+' courses)' +contentEnd+pEnd+newline +
+                   pStartLight+titleStartLight+ 'Earned at PSU' +titleEnd+newline+contentStartLight +total.uni.credits+' credits (from '+total.uni.courses+' courses)' +contentEnd+pEnd+newline +
+                   pStartLight+titleStartLight+ 'Total (w/o "No Transfer Grade Used")' +titleEnd+newline+contentStartLight +(total.all.credits-total.noTransferGradeUsed.credits)+' credits (from '+(total.all.courses-total.noTransferGradeUsed.courses)+' courses)' +contentEnd+pEnd +
+                   pStartLight+titleStartLight+ 'Total (w/ "No Transfer Grade Used")' +titleEnd+newline+contentStartLight +total.all.credits+' credits (from '+total.all.courses+' courses)' +contentEnd+pEnd;
 let clarifiedCreditsEarned = document.createElement('div');
 clarifiedCreditsEarned.style = divStyleLight;
 clarifiedCreditsEarned.innerHTML = divInnerHTML;
 document.body.appendChild(clarifiedCreditsEarned);
 
-// this fixes issues with Darkreader
+// this fixes issues with darkreader
 setInterval(function() {
     let hadDarkreaderOn = document.head.innerHTML.includes('darkreader');
     let cssText = clarifiedCreditsEarned.style.cssText;
-    if (hadDarkreaderOn && cssText.startsWith(divStyleLight)) clarifiedCreditsEarned.style.cssText = divStyleDark;
-    else if (!hadDarkreaderOn && cssText.startsWith(divStyleDark)) clarifiedCreditsEarned.style.cssText = divStyleLight;
+
+    let needsToChangeToDark = hadDarkreaderOn && cssText.startsWith(divStyleLight),
+        needsToChangeToLight = !hadDarkreaderOn && cssText.startsWith(divStyleDark);
+
+    if (needsToChangeToDark || needsToChangeToLight) {
+        clarifiedCreditsEarned.style.cssText = needsToChangeToDark ? divStyleDark : divStyleLight;
+
+        let allP = clarifiedCreditsEarned.querySelectorAll('p');
+        for (let i = 0; i < allP.length; i++) {
+            let p = allP[i];
+
+            p.style.cssText = needsToChangeToDark ? pStyleDark : pStyleLight;
+
+            let allB = p.querySelectorAll('b');
+            for (let j = 0; j < allB.length; j++) {
+                allB[j].style.cssText = needsToChangeToDark ? titleStyleDark : titleStyleLight;
+            }
+
+            let allI = p.querySelectorAll('i');
+            for (let j = 0; j < allI.length; j++) {
+                allI[j].style.cssText = needsToChangeToDark ? childColorStyleDark : childColorStyleLight;
+            }
+        }
+    }
 }, 100);
